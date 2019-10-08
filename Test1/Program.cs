3403b998f7cf284235467e8f8df6c672b39f431c
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-10-08 16:49:58 +0100

    First commit

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -6,7 +6,28 @@ namespace Test1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Creation of a string variable
+            string b = "Banana";
+
+            // Making a new object of class program to invoc its function
+            // Instead of making it static
+            Program frase = new Program();
+
+            // Call function Hello and set local variable b as parameter
+            frase.Hello(b);
+
+            // Print out the output of the programm
+            Console.WriteLine(b);
+
+        }
+
+        /// <summary>
+        /// Funtion to test out reference types in Heap
+        /// </summary>
+        /// <param name="a"> Receives a string as parameter</param>
+        public void Hello(string a)
+        {
+            a = "hi";
         }
     }
 }
